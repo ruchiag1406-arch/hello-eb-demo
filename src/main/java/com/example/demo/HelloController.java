98902debd92bb667fe6ccec814a906a0559c32ab
ruchiag1406-arch <ruchi.ag1406@gmail.com>
2026-01-07 18:02:37 +0530

    Change message to test CI/CD

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello from Elastic Beanstalk!";
+        return "Hello from Elastic Beanstalk new version v1!";
     }
 }
